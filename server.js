88fa32088996978a2636bf46bba553f691b8dd91
최준영[chris][Tech] <chrisjune@kreamcorp.com>
2025-07-15 12:09:37 +0900

    first commit

--- server.js
+++ server.js
@@ -11,12 +11,25 @@ const wss = new WebSocket.Server({ server });
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+const drawingHistory = [];
+
 wss.on('connection', (ws) => {
-  // Broadcast to all clients
+  // Send the entire drawing history to the new client
+  drawingHistory.forEach(drawData => {
+    ws.send(drawData);
+  });
+
+  // When a message is received from a client
   ws.on('message', (message) => {
+    const messageString = message.toString();
+
+    // Add the new drawing data to our history
+    drawingHistory.push(messageString);
+
+    // Broadcast the new drawing to all other clients
     wss.clients.forEach((client) => {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
+        client.send(messageString);
       }
     });
   });
